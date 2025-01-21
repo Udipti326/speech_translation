b8b96864e39b169b90ac213a7b709533c3abc064
Udipti326 <udipti.panda2004@gmail.com>
2025-01-21 12:35:54 +0530

    added nat geo video and added audio to text code

--- Audio_extract.py
+++ Audio_extract.py
@@ -1,7 +1,7 @@
 import ffmpeg
 
-video_file = "SampleVideo_640x360_2mb.mp4"
-audio_file = "output_audio.mp3"
+video_file = "Video_English.mp4"
+audio_file = "english.mp3"
 
 ffmpeg.input(video_file).output(audio_file, map='a').run(cmd='C:\\ffmpeg\\bin\\ffmpeg.exe')
 
